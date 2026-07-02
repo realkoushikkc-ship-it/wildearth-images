898041ead76089b773f8aec2fe4e3d1e861ad886
realkoushikkc-ship-it <realkoushikkc@gmail.com>
2026-07-02 16:54:49 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,13 +9,13 @@ export default function About() {
       <Navbar isLight={false} forceTransparent={false} />
 
       {/* Hero */}
-<div className="relative h-[70vh] overflow-hidden">
+<div className="relative min-h-[85vh] overflow-hidden">
   <img
     src="https://i.postimg.cc/T3ww9C6d/FB-IMG-1782990894550.jpg?auto=compress&cs=tinysrgb&fit=crop&h=900&w=1600"
     alt="Koushik Chatterjee"
-    className="w-full h-full object-cover object-[center_30%]"
+    className="w-full h-full object-cover object-bottom"
   />
-  <div className="absolute inset-0 bg-gradient-to-b from-black/40 via-transparent to-black/80" />
+  <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/10 to-transparent" />
   <div className="absolute bottom-0 left-0 right-0 p-10 lg:p-20">
     <p className="text-amber-400 text-xs tracking-[0.5em] uppercase mb-3 font-light">About</p>
     <h1
